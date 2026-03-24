9e243d96899266943b624aeb433dbd9ec9606193
MarinaHurricane <maryishch88@gmail.com>
2026-03-24 17:01:49 +0000

    first backend

---
+++ src/server.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import helmet from 'helmet';
+import pino from 'pino-http';
+
+const app = express();
+
+const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  res
+    .status(200)
+    .json({ message: `Retrieved note with ID: ${req.params.noteId}` });
+});
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  const isProd = NODE_ENV === 'production';
+  res.status(500).json({
+    message: isProd ? 'Something went wrong' : err.stack,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
