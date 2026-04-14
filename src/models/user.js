5166d4381ab29a29c27f7f3f2d4c7023bd88d94f
MarinaHurricane <maryishch88@gmail.com>
2026-04-14 18:43:29 +0100

    reset password and cloudinary

--- src/models/user.js
+++ src/models/user.js
@@ -1,35 +1,42 @@
-import { model, Schema } from "mongoose";
+import { model, Schema } from 'mongoose';
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: true,
-        minlength: 8,
-    }
-}, {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    avatar: {
+      type: String,
+      default: 'https://ac.goit.global/fullstack/react/default-avatar.jpg',
+    },
+  },
+  {
     timestamps: true,
+  },
+);
+
+userSchema.pre('save', function () {
+  if (!this.username) {
+    this.username = this.email;
+  }
 });
 
-userSchema.pre('save', function() {
-    if(!this.username) {
-        this.username = this.email;
-    }
-});
-
-userSchema.methods.toJSON = function() {
-    const obj = this.toObject();
-    delete obj.password;
-    return obj;
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
 };
 
-export const User =  model('User', userSchema);
+export const User = model('User', userSchema);
--- src/server.js
+++ src/server.js
@@ -7,9 +7,11 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { logger } from './middleware/logger.js';
 import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
+
 
 const app = express();
 
@@ -24,6 +26,7 @@ app.use(logger);
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 
